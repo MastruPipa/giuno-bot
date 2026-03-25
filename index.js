fa073b96caa81210544780dea1d6035ea31d4ffb
MastruPipa <antonio@kataniastudio.com>
2026-03-25 11:45:04 +0100

    Fix Google auth con variabili separate

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ const app = new App({
 const client = new Anthropic();
 
 // Google Auth
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS || fs.readFileSync('credentials.json'));
-const { client_secret, client_id, redirect_uris } = credentials.installed;
-const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-oAuth2Client.setCredentials(JSON.parse(process.env.GOOGLE_TOKEN || fs.readFileSync('token.json')));
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID || JSON.parse(fs.readFileSync('credentials.json')).installed.client_id,
+  process.env.GOOGLE_CLIENT_SECRET || JSON.parse(fs.readFileSync('credentials.json')).installed.client_secret,
+  'http://localhost'
+);
+oAuth2Client.setCredentials({
+  access_token: process.env.GOOGLE_ACCESS_TOKEN,
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  token_type: 'Bearer',
+});
 
 const drive = google.drive({ version: 'v3', auth: oAuth2Client });
 const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
